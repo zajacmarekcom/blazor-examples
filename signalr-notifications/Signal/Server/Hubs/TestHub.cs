0fcf92ef83a943157f595746a566538deb4cf682
Marek Zając <marek.zajac@softwaremind.com>
2023-02-14 12:09:29 +0100

    SignalR notifications

---
+++ signalr-notifications/Signal/Server/Hubs/TestHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Signal.Server.Hubs;
+
+public class TestHub : Hub
+{
+    public async Task ActionFinished(string group)
+    {
+        await Clients.Group(group).SendAsync("ReceiveNotification");
+    }
+    
+    public async Task JoinGroup(string group)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, group);
+    }
+}
